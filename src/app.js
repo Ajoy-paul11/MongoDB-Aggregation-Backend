c662ea8cf8c7838447ead6c28c2472cee8f15432
Ajoy-paul11 <ajoypaul8875@gmail.com>
2026-01-02 20:53:13 +0530

    Added Error catching middleware.

--- src/app.js
+++ src/app.js
@@ -1,5 +1,12 @@
 import express from "express";
+import { errorController } from "./middlewares/errorController.js";
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.use(errorController);
 
 export { app };
